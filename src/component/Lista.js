bace15f5c7aee1887f134d4d39f4372efcbc32c7
Hector <hector.gimenez.escurrau@gmail.com>
2023-01-01 16:02:00 -0300

    PrimerReactJsx

---
+++ src/component/Lista.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Lista extends React.Component {    
+    render() {        
+        return (      
+            <div>      
+                <ul>
+                    <li>Learn React</li>
+                    <li>Climb Mt. Everest</li>
+                    <li>Run a marathon</li>
+                    <li>Feed the dogs</li>
+                </ul>     
+            </div>  
+        );    
+    }
+}
+
+export default Lista
